c4da6df8e064d2b0794eb92e2a82ec167bbe0c4a
Erik <eploneda@yahoo.com>
2019-02-07 21:16:07 -0800

    Assigment1
    First Assignment

---
+++ js/script.js
@@ -0,0 +1,186 @@
+/*eslint-env browser*/
+
+
+// STEP 1
+/*
+var someMonth;
+function theMonth();
+var currentMonth;
+var summerMonth;
+var myLibraryFunction;
+*/
+
+// STEP 2
+/*
+2.8
+"Hi!"
+true
+*/
+
+// STEP 3
+/*
+var numExpression = 2 * (2/3) + 4;
+var numExpression = newExpression + "%" + lastExpression; 
+*/
+
+// STEP 4
+/*
+var firstName;
+var lastName;
+var address;
+var city;
+var state;
+var zipCode;
+var yourAge;
+var referralSource;
+var mayWeContactYou;
+*/
+
+// STEP 5
+/*
+
+var firstName;
+firstName = "Erik";
+var lastName;
+lastName = "Ploneda";
+var address;
+address = "123 1st Street";
+
+var city = "San Diego";
+var state = "CA";
+var zipCode = 92113;
+
+var yourAge = 33, var referralSource = true, var mayWeContactYou = false;
+
+*/
+
+// STEP 6
+/*
+var ageCar = 54;
+window.console.log("My Riviera is" + ageCar + "years old.");
+*/
+
+// STEP 7
+/*
+var x;
+var y;
+
+x = 
+y = 
+*/
+// STEP 8
+/*
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."';
+window.console.log(someString);
+
+Above comment is valid.
+*/
+
+// STEP 9
+/*
+var numName = null;
+window.console.log(numName);
+
+var y;
+window.console.log(y);
+*/
+
+// STEP 10
+/*
+window.console.log(typeof "Erik", typeof 65, typeof false, typeof [5,6,7,8],typeof myName);
+*/
+
+// STEP 11
+/*
+window.alert("Hello " +  "Erik Ploneda" + ", welcome to the JavaScript class!");
+*/
+// STEP 12
+/*
+var name = "Erik Ploneda";
+window.alert("Hello " +  name + ", welcome to the JavaScript class!");
+*/
+// STEP 13
+/*
+var name = "Erik Ploneda";
+var course = "JavaScript";
+window.alert("Hello " +  name + ", welcome to the " + course + " class!");
+*/
+
+// STEP 14
+/*
+var name = "Erik Ploneda";
+var course = "JavaScript";
+window.alert("Hello " +  name + "." + "\n" + "Welcome to the " + course + " class!");
+*/
+
+// STEP 15
+/*
+var name = prompt("What is your name?");
+var course = "JavaScript";
+window.alert("Hello " +  name + "." + "\n" + "Welcome to the " + course + " class!");
+*/
+
+// STEP 16
+/*
+var name = prompt("What is your name?");
+var course = prompt("What class are you taking?");
+window.alert("Hello " +  name + "." + "\n" + "Welcome to the " + course + " class!");
+*/
+
+// STEP 17
+/*
+var x = 10;
+var y = 20;
+window.console.log(x + y);
+*/
+
+// STEP 18
+/*
+var x = 20;
+window.console.log(x + 20);
+*/
+
+// STEP 19
+/*
+var x = 20;
+window.console.log(x * 5);
+*/
+
+// STEP 20
+/*
+var x = 20 % 3;
+window.console.log(x / 1);
+*/
+
+// STEP 21
+/*
+var x = 2;
+var y = 5;
+window.console.log(x < x || y > x);
+*/
+
+// STEP 22
+/*
+var x = 7;
+var y = 3;
+window.console.log(x == y && y == x);
+*/
+
+// STEP 23
+
+/*
+var widget = new Object();
+window.console.log(typeof widget);
+*/
+
+// STEP 24
+/*
+var widget = new Object();
+window.console.log(widget instanceof Object);
+*/
+
+/*
+// STEP 25
+var widget = new Object();
+window.console.log(widget instanceof String);
+*/
